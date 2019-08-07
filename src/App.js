5f958c4bcbc8cc0a169c501e7a26ae99c6446380
Amanda Haynes <dev@portad.ca>
2019-08-06 19:22:01 -0600

    message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [showMessage, setShowMessage] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-logo" />
         <ul className="App-nav">
-          <li className="app-nav-item-wrapper">
+          <li onClick={() => setShowMessage(!showMessage)}>
             <a className="App-nav-item">Login</a>
           </li>
         </ul>
       </header>
+      <p className={showMessage ? `App-message--show` : "App-message"}>
+        Login coming soon!
+      </p>
     </div>
   );
 }
