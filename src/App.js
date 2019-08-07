38f100b7586562d1b4581825d134e2a1b7c9d00e
Amanda Haynes <dev@portad.ca>
2019-08-06 19:15:27 -0600

    header styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="App-logo" />
+        <ul className="App-nav">
+          <li className="app-nav-item-wrapper">
+            <a className="App-nav-item">Login</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
