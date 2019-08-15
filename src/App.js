99730ce85b415067ee4e984432cc1f7ef354cf67
Amanda Haynes <dev@portad.ca>
2019-08-14 19:38:50 -0600

    save auth state in context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./Login";
+import { StateProvider } from "./StateProvider";
 
 import "./App.css";
 
 function App() {
+  const initialState = {
+    loggedIn: false
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "login":
+        return {
+          ...state,
+          loggedIn: true
+        };
+
+      case "logout":
+        return {
+          ...state,
+          loggedIn: false
+        };
+
+      default:
+        return state;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-logo" />
-        <ul className="App-nav" />
-      </header>
-      <Login />
-    </div>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo" />
+          <ul className="App-nav" />
+        </header>
+        <Login />
+      </div>
+    </StateProvider>
   );
 }
 
