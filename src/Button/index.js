8d60627e07608f9d5a2401b54467c559f3392240
Amanda Haynes <dev@portad.ca>
2019-08-25 18:43:09 -0600

    Notifications and property sections

---
+++ src/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+
+export const ButtonHollow = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
+};
