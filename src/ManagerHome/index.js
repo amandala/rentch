7c353d094344db429b71dc78c5853382d9ad5153
Amanda Haynes <dev@portad.ca>
2019-09-10 22:29:42 -0600

    manager view notifications

--- src/ManagerHome/index.js
+++ src/ManagerHome/index.js
@@ -12,32 +12,40 @@ import "./index.scss";
 const ManagerHome = ({ properties }) => {
   const [{ userData }, dispatch] = useStateValue();
 
-  // const renderNotifications = () => {
-  //   return filteredNotifications.map(notification => {
-  //     console.log(notification);
-  //     const { date, type, subject } = notification.fields;
+  const renderNotifications = properties => {
+    const notifications = properties
+      .flatMap(property => property.fields.notifications)
+      .filter(notification => notification !== undefined);
 
-  //     return (
-  //       <div className="Notification">
-  //         <span>{date}</span>
-  //         <span>{type}</span>
-  //         <span>{subject}</span>
-  //       </div>
-  //     );
-  //   });
-  // };
+    console.log(notifications);
+
+    if (notifications.length) {
+      return (
+        <div className="Notifications">
+          <HeadingMedium>You have new notifications</HeadingMedium>
+          {notifications.map(notification => {
+            const { date, type, subject } = notification.fields;
+            return (
+              <div className="Notification">
+                <span>{date}</span>
+                <span>{type}</span>
+                <span>{subject}</span>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+  };
 
   return (
     <div className="ManagerHome">
       <div className="ManagerGreeting">
         <HeadingLarge>Welcome, {userData.givenName}</HeadingLarge>
       </div>
-      {/* {filteredNotifications.length > 0 ? (
-        <div className="Notifications">
-          <HeadingMedium>You have new notifications</HeadingMedium>
-          {renderNotifications()}
-        </div>
-      ) : null} */}
+
+      {renderNotifications(properties)}
+
       <div>
         {properties.map(property => (
           <Property property={property} />
