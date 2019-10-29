9640a8ec398297008fbed4a5c2c76bb1ee5d3021
Amanda Haynes <dev@portad.ca>
2019-10-28 21:45:48 -0600

    tenant response form started

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { useModal } from "react-modal-hook";
 import {
@@ -9,33 +9,77 @@ import {
   DialogContent,
   DialogContentText
 } from "@material-ui/core";
+import { Form, TextArea, useFormState, Select, Option } from "informed";
 
 import { useStateValue } from "../../StateProvider";
 
 import styles from "./index.module.scss";
 
+const RequestBuilder = ({ hideModal }) => {
+  const [succes, setSuccess] = useState(false);
+  const formState = useFormState();
+
+  const { submits, errors, values } = formState;
+
+  if (submits === 1 && !errors.length && !succes) {
+    console.log(values);
+
+    hideModal();
+
+    // buildRequest(property.property, values).then(data => {
+    //   if (data.error) {
+    //     // error notification
+    //     console.error("There was an error", data.error);
+    //   }
+
+    //   setSuccess(true);
+    //   // success notification
+    //   // redirect to home page
+    // });
+  }
+
+  // if (succes) {
+  //   return <Redirect to="/" />;
+  // }
+
+  return null;
+};
+
 const Notification = ({ notification }) => {
+  const [showModal, hideModal] = useModal(({ in: open, onExited }) => (
+    <Dialog fullScreen open={open} onExited={onExited}>
+      <DialogTitle>{notification.fields.subject}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{notification.fields.message}</DialogContentText>
+
+        <Form>
+          <label>
+            <TextArea
+              className={styles.Field}
+              field="response"
+              validate={validate}
+            />
+          </label>
+        </Form>
+        <DialogActions>
+          <Button type="submit">Send</Button>
+          <Button onClick={hideModal}>Close</Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  ));
+
   const [{ userData }] = useStateValue();
 
   const date = Date.parse(notification.fields.date);
   const formattedDate = moment(date).format("ll");
   const formattedTime = moment(date).format("LT");
 
-  console.log(notification);
-
   const isCreator = notification.fields.creator.fields.email === userData.email;
 
-  const [showModal, hideModal] = useModal(({ in: open, onExited }) => (
-    <Dialog fullScreen open={open} onExited={onExited}>
-      <DialogTitle>{notification.fields.subject}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>{notification.fields.message}</DialogContentText>
-        <DialogActions>
-          <Button onClick={hideModal}>Close</Button>
-        </DialogActions>
-      </DialogContent>
-    </Dialog>
-  ));
+  const validate = value => {
+    return !value || !value.length > 1 ? "This field is required" : undefined;
+  };
 
   return (
     <button
@@ -50,6 +94,7 @@ const Notification = ({ notification }) => {
       <span>{`${isCreator ? "Outgoing " : "Incomming "}${
         notification.fields.type
       }`}</span>
+      <RequestBuilder hideModal={hideModal} />
     </button>
   );
 };
