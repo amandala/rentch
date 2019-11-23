d4d6e49d8e6c2320216abf7caa73e1234233338b
Amanda Haynes <dev@portad.ca>
2019-11-23 14:05:12 -0700

    Tenant and Manager home styling

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,17 +1,32 @@
 import React from "react";
+import cx from "classnames";
+
+import styles from "./index.module.scss";
 
 export const HeadingLarge = ({ children }) => {
-  return <h1>{children}</h1>;
+  return (
+    <h1 className={cx(styles.Heading, styles.HeadingLarge)}>{children}</h1>
+  );
 };
 
 export const HeadingMedium = ({ children }) => {
-  return <h2>{children}</h2>;
+  return (
+    <h2 className={cx(styles.Heading, styles.HeadingMedium)}>{children}</h2>
+  );
 };
 
 export const HeadingSmall = ({ children }) => {
-  return <h3>{children}</h3>;
+  return (
+    <h3 className={cx(styles.Heading, styles.HeadingSmall)}>{children}</h3>
+  );
 };
 
 export const HeadingXSmall = ({ children }) => {
-  return <h4>{children}</h4>;
+  return (
+    <h4 className={cx(styles.Heading, styles.HeadingXSmall)}>{children}</h4>
+  );
+};
+
+export const Text = ({ children, className }) => {
+  return <p className={cx(styles.Text, className)}>{children}</p>;
 };
