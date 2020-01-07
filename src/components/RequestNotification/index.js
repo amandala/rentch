6fd91df4ca3b23b9a0295c08c407986942f8df2a
Amanda Haynes <dev@portad.ca>
2020-01-07 16:24:52 -0700

    variablize all colors

--- src/components/RequestNotification/index.js
+++ src/components/RequestNotification/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import { HeadingXSmall } from "../Heading";
+
 import styles from "./index.module.scss";
 
 import { getFormattedDate } from "../../helpers/getFormattedDate";
@@ -9,12 +11,18 @@ import { getFormattedDate } from "../../helpers/getFormattedDate";
 const RequestNotification = ({ request }) => {
   return (
     <Link to={`/request/${request.sys.id}`} className={styles.Wrapper}>
-      <span
-        className={styles.Subject}
-      >{`${request.fields.status} ${request.fields.type} ${request.fields.property.fields.name}`}</span>
-      <span className={styles.Date}>
-        {getFormattedDate({ date: request.sys.updatedAt })}
+      <span>
+        <HeadingXSmall className={styles.Date}>
+          {getFormattedDate({ date: request.sys.updatedAt })}
+        </HeadingXSmall>
+        <span className={styles.Subject}>
+          {request.fields.property.fields.name}
+        </span>
       </span>
+      <div className={styles.Status}>
+        <span>{request.fields.status}</span>
+        <span>{request.fields.type}</span>
+      </div>
     </Link>
   );
 };
