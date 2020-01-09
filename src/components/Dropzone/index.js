d080be56e08a5bdd9b326fb8c2469b31a1615946
Amanda Haynes <dev@portad.ca>
2020-01-09 15:37:47 -0700

    add eslint-config-prettier and modified some rules w/ associated fixes

--- src/components/Dropzone/index.js
+++ src/components/Dropzone/index.js
@@ -68,7 +68,7 @@ const processFile = (file, setUploadState) => {
     })
     .catch(err => {
       setUploadState("Error");
-      console.log(err);
+      console.error(err);
     });
 };
 
@@ -84,8 +84,8 @@ const MyDropzone = () => {
       acceptedFiles.forEach(file => {
         const reader = new FileReader();
 
-        reader.onabort = () => console.log("file reading was aborted");
-        reader.onerror = () => console.log("file reading has failed");
+        reader.onabort = () => console.warn("file reading was aborted");
+        reader.onerror = () => console.warn("file reading has failed");
         reader.onload = () => {
           processFile(file, setUploadState).then(result => {
             dispatch({
