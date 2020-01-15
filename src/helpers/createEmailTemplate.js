4f8b38a2a005e354b6acc2868d7c8e2ef4600e6c
Amanda Haynes <dev@portad.ca>
2020-01-15 09:29:14 -0700

    default message

--- src/helpers/createEmailTemplate.js
+++ src/helpers/createEmailTemplate.js
@@ -3,30 +3,35 @@ import getPrettyRequestType from "./getPrettyRequestType";
 const getEmailSubject = (property, type, recipient, repairOwner, status) => {
   const followup = `${property.fields.name}: The tenant still needs help`;
   const closed = `${property.fields.name}: The ${type} request has been closed`;
-  const managerRepair = `${property.fields.name}:  The landlord has requested help from Rentch for the ${getPrettyRequestType(
+  const managerRepair = `${
+    property.fields.name
+  }:  The landlord has requested help from Rentch for the ${getPrettyRequestType(
     type
   ).toLowerCase()}`;
   const landlordRepair = `${property.fields.name}: The ${getPrettyRequestType(
     type
   ).toLowerCase()} will be handled by your landlord`;
+  const defaultMessage = `${property.fields.name}: There is new activity on the ${type}`;
 
-  switch (status){
+  switch (status) {
     case "followup":
       return followup;
     case "closed":
       return closed;
     case "repair":
-      if(recipient.fields.role === "tenant"){
+      if (recipient.fields.role === "tenant") {
         if (repairOwner === "manager") {
           return managerRepair;
         }
         return landlordRepair;
+      } else if (recipient.fields.role === "manager") {
+        return managerRepair;
       }
-      else if(recipient.fields.role === "manager") {
-        return managerRepair
-      }
+      break;
+    default:
+      return defaultMessage;
   }
-}
+};
 
 const createEmailTemplate = (
   creator,
