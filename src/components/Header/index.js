067e6f3d9b1162088923d772b42e39bc63c53d6f
Amanda Haynes <dev@portad.ca>
2020-01-28 19:27:29 -0700

    auth0 sdk integration started

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,25 @@
 import React from "react";
 import { withRouter } from "react-router";
+import { useAuth0 } from "../../react-auth0-spa";
 
-import { useStateValue } from "../../StateProvider";
-import { ButtonText } from "../Button";
+// import { useStateValue } from "../../StateProvider";
+// import { ButtonText } from "../Button";
 
 import styles from "./index.module.scss";
 
 const Header = props => {
-  const [{ loggedIn }, dispatch] = useStateValue();
+  const { isAuthenticated, loginWithRedirect, logout, loading } = useAuth0();
+
+  // const [{ loggedIn }, dispatch] = useStateValue();
   return (
     <header className={styles.Header}>
       <div className={styles.Logo} onClick={() => props.history.push("/")} />
-      {loggedIn ? (
-        <ButtonText
-          onClick={() => {
-            props.history.push("/login");
-            dispatch({
-              type: "LOGOUT"
-            });
-            localStorage.removeItem("profile");
-          }}
-        >
-          Logout
-        </ButtonText>
-      ) : null}
+      {loading ? "LOADING..." : null}
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
     </header>
   );
 };
